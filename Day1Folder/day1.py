cedcc6cc64b8ea873243ec119ab68f5a197ef340
snaggoh <photosgohkids@gmail.com>
2025-06-17 01:22:40 +0000

    Jun 17, 2025, 1:22 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,41 +1,62 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day1")
-
+print("My name is Hannah.")
+print("I am 9 years old.")
+print("I go to Peihwa resbyterian Primary School.")
 ########################################################################
 # Task 1:
-
-
-
+wallet = 1000
+print( wallet + 3.50 ) 
+print( wallet - 3.50 ) 
+print( wallet * 3.50 ) 
 ########################################################################
 # Task 2:
+print("I would give 1 quater to my parents and use the rest to fly to Japan")
 
 
 
 ########################################################################
 # Task 3:
-
-
-
+animal1 = "Tiger"
+animal2 = "Cheetah"
+animal3 = "Monkey"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
+variable1 = 3
+variable2 = 5
+print(variable1 + variable2) 
+name = "Hannah" 
+message = "Let's go to MacDonald's"
+print(name + " says " + message)
 
 ########################################################################
 # Task 5:
-
+name = "Disco"
+food = "meat"
+print(" My dog's name is " + name + "." )
+print("She likes to eat " + food + "." )
+      
 
 
 ########################################################################
 # Task 6:
-
-
-
+def intro(newFriend):
+    print("My name is Hannah.")
+    print("I am 9 years old.")
+    print("I go to Peihwa Presbyterian Primary School.")
+    print("nice to meet you, " + newFriend)
+intro("Mary")
+intro("Peter")
 ########################################################################
 # Task 7:
-
-
-
+def addition(num1, num2):
+    result = num1 + num2
+    print(result)
+addition(100,3)
 ########################################################################
 # Additional exercises:
+
