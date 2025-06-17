525cbe293c0388e1e9fcc079eb880e64c4ed0941
snaggoh <photosgohkids@gmail.com>
2025-06-17 02:09:34 +0000

    Jun 17, 2025, 2:09 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -12,7 +12,7 @@ for letter in "This is a beautiful day":
 # Task 2:
 name = "Phoebe"
 for letter in name:
-    print("")
+    print("Give me a ")
 ########################################################################
 # Task 3:
 
