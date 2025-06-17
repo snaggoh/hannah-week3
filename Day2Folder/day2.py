5fd8319f22e8c5a64e08e556c33a28fb1da7c378
snaggoh <photosgohkids@gmail.com>
2025-06-17 01:52:27 +0000

    Jun 17, 2025, 1:52 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -4,7 +4,7 @@ print("hello from day2")
 
 ########################################################################
 # Task 1:
-for count in range(10):
+for count in range(100000000):
     print("I will not sling mud at my friends!")
 
 ########################################################################
